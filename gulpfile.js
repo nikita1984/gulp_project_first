6b0b9e0c28a1580cd57641885e61dab9cc2a9a93
nikita1984 <nikitabessonov.ru@gmail.com>
2022-06-21 16:57:57 +0300

    browserSync completed

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,36 @@ const { series, parallel, src, dest, watch } = require('gulp');
 // подключаем плагины
 const sass = require('gulp-sass')(require('sass'));
 const pug = require('gulp-pug');
+const browserSync = require('browser-sync').create();
 
-// декларируем задачи шаблонизатора и препроцессинга
+// декларируем задачи
 function views() {
     return src('./app/views/*.pug')
         .pipe(pug({pretty:true}))
-        .pipe(dest('./dist/'));
+        .pipe(dest('./dist/'))
+        .pipe(browserSync.stream());
 }
-
 function preprocessing () {
     return src('./app/sass/*.sass')
       .pipe(sass().on('error', sass.logError))
-      .pipe(dest('./dist/css'));
+      .pipe(dest('./dist/css'))
+      .pipe(browserSync.stream());
+      /* выражение аналогично pipe(browserSync.stream())
+      .pipe(browserSync.reload({
+        stream: true
+      }));
+      */
 }
 
-// регистрируем задачу по шаблонизации и препроцессингу с отслеживанием файлов
-exports.viewsWatch = function() {
-        watch('./app/views/*.pug', views);
-    };
-    
-exports.sassWatch = () => {
-        watch('./app/sass/*.sass', preprocessing)
-    };    
+function browserInit () {
+    browserSync.init({
+        server: {
+            baseDir: 'dist'
+        },
+        });
+
+    watch('./app/sass/*.sass', preprocessing);
+    watch('./app/views/*.pug', views);    
+}
+
+exports.browserInit = browserInit; 
