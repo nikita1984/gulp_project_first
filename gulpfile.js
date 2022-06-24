1923b7fc435ac979bc76326ba7c84224d11597ca
nikita1984 <nikitabessonov.ru@gmail.com>
2022-06-24 11:28:55 +0300

    realised gulp-if and gulp-uglify functions

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ const { series, parallel, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const pug = require('gulp-pug');
 const browserSync = require('browser-sync').create();
+const useref = require('gulp-useref');
+const uglify = require('gulp-uglify');
+const gulpIf = require('gulp-if');
 
 // декларируем задачи
 function views() {
@@ -25,7 +28,11 @@ function preprocessing () {
       */
 }
 function htmlTransfer () {
+    // return src('./app/index.html')
+    // .pipe(dest('./dist/'))
+    // .pipe(browserSync.stream());
     return src('./app/index.html')
+    .pipe(useref())
     .pipe(dest('./dist/'))
     .pipe(browserSync.stream());
 }
@@ -36,6 +43,22 @@ function cssTransfer () {
     .pipe(browserSync.stream());
 }
 
+function userefFunction () {
+    return src('./app/index.html')
+    .pipe(useref())
+    .pipe(dest('./dist/'))
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(browserSync.stream());
+}
+
+/*
+function jsTransfer () {
+    return src('./app/js/*.js')
+    .pipe(dest('./dist/js/'))
+    .pipe(browserSync.stream());
+}
+*/
+
 function browserInit () {
     browserSync.init({
         server: {
@@ -45,8 +68,10 @@ function browserInit () {
 
     watch('./app/sass/*.sass', preprocessing);
     watch('./app/views/*.pug', views);
-    watch('./app/index.html', htmlTransfer);
-    watch('./app/css/*.css', cssTransfer);        
+    watch('./app/index.html', userefFunction);
+    watch('./app/css/*.css', cssTransfer);
+    watch('./app/js/**/*.js', userefFunction);          
 }
 
 exports.default = browserInit; 
+// exports.useref = userefFunction;
